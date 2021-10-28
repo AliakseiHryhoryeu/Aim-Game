3c0bc0d21f8f111e0953b526f389b4e9d9a113a1
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-10-28 18:53:42 +0300

    Update game
    use sass instead of scss, add new functional

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
 const timeEl = document.querySelector('#time');
 const board = document.querySelector('#board');
-const colors = ['#fbf2eb', '#2e5e84', '#bbd70b', '#fa9705', '#d7beac'];const screenWidth = window.screen.width
-
 let time = 0;
 let score = 0;
 
@@ -14,77 +12,88 @@ startBtn.addEventListener('click', (event) => {
     screens[0].classList.add('up');
 });
 
-timeList.addEventListener('click', event => {
-    if (event.target.classList.contains('time-btn')) {
-        time = parseInt(event.target.getAttribute('data-time'))
-        screens[1].classList.add('up')
-        startGame()
+timeList.addEventListener('click', (event) => {
+    if(event.target.classList.contains('time-btn')) {
+       time = parseInt(event.target.getAttribute('data-time'));
+       screens[1].classList.add('up');
+       startGame();
     }
-})
-board.addEventListener('click',event=>{
+});
+
+board.addEventListener('click', event => {
     if(event.target.classList.contains('circle')){
-        score++
-        event.target.remove()
-        createRandomCircle()
+        score++;
+        setTimeout(event.target.remove(), 1000);
+        createRandomCircle();
     }
-})
-function getRandomColor(){
-    const color = Math.floor(Math.random()*colors.length)
-    return colors[color]
-}
-function setColor(element){
-    const color = getRandomColor()
-    element.style.background=color
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
-}
+});
 
 
 function startGame() {
-    setInterval(decreasTime, 1000)
-    createRandomCircle()
-    setTime(time)
+    setInterval(decreaseTime, 1000);
+    createRandomCircle();
+    setTime(time);
 }
 
-function decreasTime() {
-    if (time === 0) {
-        finishGame()
-    } else {
-        let current = --time
-        if (current < 10) {
-            current = `0${current}`
+function decreaseTime(){
+    if(time === 0){
+        finishGame();
+    }else{
+        let curent = --time;
+        if(curent<10){
+            curent= `0${curent}`;
+            timeEl.style.color = 'red';
         }
-        setTime(current)
+        setTime(curent);
     }
 }
 
-function setTime(value) {
-    timeEl.innerHTML = `00:${value}`
+function setTime(value){
+    return timeEl.innerHTML = `00:${value}`;
 }
 
-function finishGame() {
-    timeEl.parentNode.classList.add('hide')
-board.innerHTML=`<h1>Счет: <span class="primary">${score}</span></h1>`
+function finishGame()
+{
+    board.innerHTML = `<h1>Ваш счет: <span class="primary">${score}</span></h1><br>`;
+    board.innerHTML += '<a href="" class="start">Заново</a>';
+    timeEl.parentElement.style.opacity = '0';
+    
 }
 
-function createRandomCircle() {
-    const circle = document.createElement('div')
-    const size = getRandomNumber(10,60)
-    const {width,height} = board.getBoundingClientRect()
-    const x = getRandomNumber(0,width-size)
-    const y = getRandomNumber(0,height-size)
-
-    circle.classList.add('circle')
-    circle.style.width = `${size}px`
-    circle.style.height = `${size}px`
-    circle.style.top = `${y}px`
-    circle.style.left = `${x}px`
-    setColor(circle)
-
-    board.append(circle)
+function createRandomCircle(){
+    const circle = document.createElement('div');
+    const size = `${getRandomInt()}px`;
+    circle.classList.add('circle');
+    const x = circle.style.left = `${getRandomInt(3,95)}%`;
+    const y = circle.style.top = `${getRandomInt(5,95)}%`;
+   
+    circle.style.width = size;
+    circle.style.height = size;
+    setColor(circle);
+    board.append(circle);
+    setInterval(fly,300, circle)
+    setInterval(setColor,800, circle)
 }
 
-function getRandomNumber(min, max) {
-return Math.round(Math.random()*(max-min)+min)
+function setColor(elem){
+		let i = 1;
+    
+    
+    let r = getRandomInt(min = 0, max = 255),g = getRandomInt(min = 0, max = 255),b = getRandomInt(min = 0, max = 255);
+    let color = `rgb(${r},${g},${b})`;
+    return elem.style.background = color, elem.style.boxShadow = `0 0 10px ${color}, 0 0 25px ${color}`;
+    
 }
+
+function getRandomInt(min = 25, max = 45) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+ function fly(elem){
+elem.style.transform = `translateY(${getRandomInt(-100,100)}px)`
+elem.style.transform = `translateX(-${getRandomInt(-100,100)}px)`
+ }
+  
+  
 
 
